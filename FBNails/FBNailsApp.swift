4212719eeab5ceb180cd926944b60cf624487f50
Daniele Sebastio <daniele.sebastio@capgemini.com>
2024-05-25 14:05:09 +0200

    initial commit

---
+++ FBNails/FBNailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBNailsApp.swift
+//  FBNails
+//
+//  Created by Daniele Sebastio on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FBNailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
